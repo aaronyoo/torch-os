e610e695039ebd77bdfb1c62ed9e6df510b9efba
Aaron <atyoo99@gmail.com>
2019-03-07 22:17:27 -0800

    add interrupt descriptor table

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,8 +1,90 @@
+#include <stdint.h>
+#include <stddef.h>
+
+struct _idt_entry {
+	uint16_t baseLo;
+	uint16_t sel;
+	uint8_t res;
+	uint8_t flags;
+	uint16_t baseHi;
+} __attribute__((packed));
+typedef struct _idt_entry idt_entry;
+
+struct _idt_ptr {
+	uint16_t limit;
+	uint32_t base;
+} __attribute__((packed));
+typedef struct _idt_ptr idt_ptr;
+
+#define IDT_ENTRIES 256
+static idt_entry idt[IDT_ENTRIES];
+static idt_ptr idtr;
+
+void print(char *s);
+
+static void idt_install() {
+	__asm__(
+		"lidt (idtr)\n"
+	);
+}
+
+void* memset(void* bufptr, int value, size_t size) {
+	unsigned char* buf = (unsigned char*) bufptr;
+	for (size_t i = 0; i < size; i++)
+		buf[i] = (unsigned char) value;
+	return bufptr;
+}
+
+void i86_default_handler() {
+	print("Unhandled Exception\n");
+	print("Hanging...\n");
+	for (;;);
+}
+
+void install_handler(uint8_t n, uint32_t base, uint16_t sel, uint8_t flags) {
+	idt[n].baseLo = base & 0xFFFF;
+	idt[n].baseHi = (base >> 16) & 0xFFFF;
+	idt[n].sel = sel;
+	idt[n].res = 0;
+
+	// need to | 0x60 later
+	idt[n].flags = flags;
+}
+
+int init_idt() {
+	idtr.limit = sizeof(idt_entry) * IDT_ENTRIES;
+	idtr.base = (uint32_t)idt; 
+
+	memset(idt, 0, sizeof(idt_entry) * IDT_ENTRIES);
+
+	for (int i = 0; i < IDT_ENTRIES; i++) {
+		install_handler(i, (uint32_t) i86_default_handler, 0x08, 0x8E);
+	}
+
+	idt_install();
+}
+
+// int i86_install_ir(uint32_t i, uint16_t flags, uint16_t sel, I)
+
+// Temporary Screen Printing Code
+short* vga_ptr = (short*)0xb8000;
+const short color = 0x0F00;
+
+void puts(char c) {
+	*vga_ptr = color | c;
+	vga_ptr++;
+}
+
+void print(char *s) {
+	while (*s != '\0' && *s != '\n') {
+		puts(*s);
+		s++;
+	}
+}
+
 void kernel_main()
 {
-	const short color = 0x0F00;
-	const char* hello = "Hello cpp world!";
-	short* vga = (short*)0xb8000;
-	for (int i = 0; i<16;++i)
-		vga[i+80] = color | hello[i];
+	print("About to cause an interrupt..\n");
+	init_idt();
+	__asm__("int3");
 }
